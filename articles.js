44c19e73d53250ac37b8064077367f0a9706397b
henrylong <henry.cassetta.long@gmail.com>
2018-11-28 18:35:07 -0500

    adds most of remaining content, minor styling changes

--- articles.js
+++ articles.js
@@ -6,26 +6,138 @@ let articles = [
             {
                 src: 'resources/catnetlogo_white_512.png',
                 alt: 'logo_red',
-                label: 'label',
             },
             {
                 src: 'resources/catnetlogo_black512.png',
                 alt: 'logo_red',
-                label: 'label',
             }
         ],
         content: 'My target client is an on campus organization that I am part of, Stony Brook Cat Network. The organization focuses on taking care of the many feral cats on campus, keeping them fed and healthy. For most people outside of the organization, their first experience with us is them stumbling across our many cat shelters and feeding stations located all around campus. As such, that became the focal point of my design. It is a bold and simple design. The solid and angular nature gives the impression that we are dependable and responsible, but the rounded corners and bright red color tell the viewer that we are engaging and friendly.',
     },
     {
-        title: 'sampleTitle2',
-        date: 'sampleDate2',
+        title: 'Typography',
+        date: '10-01-18',
         images: [
             {
-                src: 'https://via.placeholder.com/150',
-                alt: 'alt',
-                label: 'label',
+                src: 'resources/catnettypo_red_512.png',
+                alt: 'typo_red',
+            },
+            {
+                src: 'resources/catnettype_black_512.png',
+                alt: 'typo_black',
+            },
+            {
+                src: 'resources/sbucat_fontsample_small.png',
+                alt: 'typo_black',
             }
         ],
-        content: 'sampleContent2',
-    }
+        content: 'Stony Brook Cat Network is an on campus organization that provides reliable and robust services to the Stony Brook community. As such, I decided to use fonts that reinforced that idea. DDC Hardware by the Draplin Design Company is a bold font that presents itself as sturdy and hard working, and as such was the ideal font to use for this typographic logo, as well as any headers. In addition, Roboto Slab will be used for body text. This was chosen because its serifs provide readability, while the overall feel of the font helps to support the aforementioned ideas of robustness.',
+    },
+    {
+        title: 'Icons',
+        date: '10-12-18',
+        images: [
+            {
+                src: 'resources/icons/color/1.png',
+                alt: 'icon_1',
+            },
+            {
+                src: 'resources/icons/color/2.png',
+                alt: 'icon_2',
+            },
+            {
+                src: 'resources/icons/color/3.png',
+                alt: 'icon_3',
+            },
+            {
+                src: 'resources/icons/color/4.png',
+                alt: 'icon_4',
+            },
+            {
+                src: 'resources/icons/black/1.png',
+                alt: 'icon_1',
+            },
+            {
+                src: 'resources/icons/black/2.png',
+                alt: 'icon_2',
+            },
+            {
+                src: 'resources/icons/black/3.png',
+                alt: 'icon_3',
+            },
+            {
+                src: 'resources/icons/black/4.png',
+                alt: 'icon_4',
+            }
+        ],
+        content: 'My target client is an on campus organization that I am part of, Stony Brook Cat Network. The organization focuses on taking care of the many feral cats on campus, keeping them fed and healthy. The selected icons were created to facilitate navigation through the Cat Network website. The first, a winter shelter, is a scaled down version of the logo sans text. This will denote a tab which users can use to navigate to a map of our campus shelters. The next, an alteration of the instagram logo, will lead users towards our instagram page where we post images and advertise events. The third is a spreadsheet, which will direct users towards our feeding schedule and feeder sign-up pages. The last is a trail camera, which will lead users towards a gallery of images taken by our automatic trail camera set up periodically near our shelters.',
+    },
+    {
+        title: 'Animation',
+        date: '10-19-18',
+        images: [
+            {
+                src: 'resources/sbucat_splash.gif',
+                alt: 'splash',
+            }
+        ],
+        content: 'My target client, Stony Brook Cat Network, just revitalized its old Instagram page. In order to prompt site visitors to seek out the Instagram page, it was made the subject of this animated splash screen. Created with Google Web Designer.',
+    },
+    {
+        title: 'Wireframe',
+        date: '11-03-18',
+        images: [
+            {
+                src: 'resources/wireframe.png',
+                alt: 'wireframe',
+            },
+            {
+                src: 'resources/wireframe_mobile.png',
+                alt: 'wireframe_mobile',
+            }
+        ],
+        content: '',
+    },
+    {
+        title: 'Responsive_Web_Design',
+        date: '11-23-18',
+        images: [
+            {
+                src: 'resources/responsive/web_home.png',
+                alt: 'web',
+            },
+            {
+                src: 'resources/responsive/web_map.png',
+                alt: 'web',
+            },
+            {
+                src: 'resources/responsive/web_faq.png',
+                alt: 'web',
+            },
+            {
+                src: 'resources/responsive/mobile_home.png',
+                alt: 'mobile',
+            },
+            {
+                src: 'resources/responsive/mobile_map.png',
+                alt: 'mobile',
+            },
+            {
+                src: 'resources/responsive/mobile_faq.png',
+                alt: 'mobile',
+            },
+        ],
+        content: '',
+    },
+    {
+        title: 'Character_Design',
+        date: '11-03-18',
+        images: [
+            {
+                src: 'resources/wireframe.png',
+                alt: 'wireframe',
+            },
+        ],
+        content: '',
+    },
 ];
--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-for(let i = 0; i < articles.length; i++){
-    let images = '<div class="imageContainer">';
-    for(let j = 0; j < articles[i].images.length; j++){
-        images += '<img src="'
-        + articles[i].images[j].src
-        + '" alt="'
-        + articles[i].images[j].alt
-        + '" />';
+$(document).ready(() => {
+    for(let i = 0; i < articles.length; i++){
+        let images = '<div class="imageContainer">';
+        for(let j = 0; j < articles[i].images.length; j++){
+            images += '<img src="'
+            + articles[i].images[j].src
+            + '" alt="'
+            + articles[i].images[j].alt
+            + '" />';
+        }
+        images += '</div>';
+    
+        $(".articleContainer").append('<div id="spacer'
+        + articles[i].title
+        + '" style="height: 15vh;"></div><div id="'
+        + articles[i].title
+        + '" class="article"><h2>'
+        + articles[i].title
+        + '</h2><h3>'
+        + articles[i].date
+        + '</h3>'
+        + images
+        + '<p>'
+        + articles[i].content
+        + '</p></div>');
+    
+        $(".nav").append('<button id="nav'
+        + articles[i].title
+        + '">'
+        + articles[i].title + '<br>' + articles[i].date
+        + '</button>');
+        $("#nav"+articles[i].title).click(() => {
+            $("#spacer" + articles[i].title)[0].scrollIntoView();
+        });
     }
-    images += '</div>';
-
-    $(".articleContainer").append('<div class="article"><h2>'
-    + articles[i].title
-    + '</h2><h3>'
-    + articles[i].date
-    + '</h3>'
-    + images
-    + '<p>'
-    + articles[i].content
-    + '</p></div>');
-}
+});
