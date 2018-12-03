c05541b5683e02df394e213a76a7ffaf257548dc
henrylong <henry.cassetta.long@gmail.com>
2018-12-03 13:18:54 -0500

    added updated logo

--- articles.js
+++ articles.js
@@ -10,6 +10,10 @@ let articles = [
             {
                 src: 'resources/catnetlogo_black512.png',
                 alt: 'logo_red',
+            },
+            {
+                src: 'resources/sbucat_sticker.png',
+                alt: 'logo_red',
             }
         ],
         content: 'My target client is an on campus organization that I am part of, Stony Brook Cat Network. The organization focuses on taking care of the many feral cats on campus, keeping them fed and healthy. For most people outside of the organization, their first experience with us is them stumbling across our many cat shelters and feeding stations located all around campus. As such, that became the focal point of my design. It is a bold and simple design. The solid and angular nature gives the impression that we are dependable and responsible, but the rounded corners and bright red color tell the viewer that we are engaging and friendly.',
